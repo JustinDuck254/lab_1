3c25d596fb9aa096e23d6334adeea14ec824d5d0
JustinDuck254 <phulong2504.school@gmail.com>
2025-05-26 08:20:50 +0700

    Main cpp file

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	float math, science,english,average=0.0;
+	string  name
+	short age, student_id;
+	char grade;
+	cout<<"Enter your student ID: ";
+	cin>> student_id;
+	cout<<"Enter your full name: ";
+	getline(cin,name);
+	cout<<"Enter your age: ";
+	cout<<
+}
